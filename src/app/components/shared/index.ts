1e1991f7576bb778eeb68530585944d4d24a6c33
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-03-10 16:30:00 +0000

    feat: add shared components barrel export

---
+++ src/app/components/shared/index.ts
@@ -0,0 +1,19 @@
+export { Badge } from "./Badge";
+export { Button } from "./Button";
+export { CTAButton } from "./CTAButton";
+export { SectionTitle } from "./SectionTitle";
+export { GlassCard } from "./GlassCard";
+export { TeamMemberCard } from "./TeamMemberCard";
+export { SupporterCard } from "./SupporterCard";
+export { FeatureShowcase } from "./FeatureShowcase";
+export { AlumniCarouselSection } from "./AlumniCarouselSection";
+export { CardGridSection } from "./CardGridSection";
+export { CheckCardGridSection } from "./CheckCardGridSection";
+export { BulletListWithTitleSection } from "./BulletListWithTitleSection";
+export { TwoCardCTASection } from "./TwoCardCTASection";
+export { Hero } from "./Hero";
+
+export type { AlumniMember } from "./AlumniCarouselSection";
+export type { CheckCardItem } from "./CheckCardGridSection";
+export type { FeatureShowcaseProps } from "./FeatureShowcase";
+export type { HeroProps, HeroVariant } from "./Hero";
