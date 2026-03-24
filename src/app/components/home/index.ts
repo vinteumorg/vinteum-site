ebd810f71f5122a30617adc87518878f3276de83
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-03-24 15:30:00 +0000

    feat: add home components barrel export

---
+++ src/app/components/home/index.ts
@@ -0,0 +1,6 @@
+export { HeroSection } from "./HeroSection";
+export { SubHeroSection } from "./SubHeroSection";
+export { SupportsSection } from "./SupportsSection";
+export { CommunitySection } from "./CommunitySection";
+export { ProgramsSection } from "./ProgramsSection";
+export { UpdatesSection } from "./UpdatesSection";
