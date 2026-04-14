6e56b7cc99d312a1f2d959c2275b56ec6aa250ba
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-04-14 15:00:00 +0000

    feat: add launchpad components barrel export

---
+++ src/app/components/launchpad/index.ts
@@ -0,0 +1,8 @@
+export { LaunchpadAboutSection } from "./LaunchpadAboutSection";
+export { LaunchpadAlumniSection } from "./LaunchpadAlumniSection";
+export { LaunchpadCohortSection } from "./LaunchpadCohortSection";
+export { LaunchpadFAQSection } from "./LaunchpadFAQSection";
+export { LaunchpadProgramStructureSection } from "./LaunchpadProgramStructureSection";
+export { LaunchpadTestimonialsSection } from "./LaunchpadTestimonialsSection";
+export { LaunchpadTimelineSection } from "./LaunchpadTimelineSection";
+export { LaunchpadWhoItsForSection } from "./LaunchpadWhoItsForSection";
