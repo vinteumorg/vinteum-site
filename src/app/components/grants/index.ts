3e97a0695843d5af99105e10c358010a608cc7dd
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-04-28 12:00:00 +0000

    feat: add grants components barrel export

---
+++ src/app/components/grants/index.ts
@@ -0,0 +1,4 @@
+export { GrantsAlumniSection } from "./GrantsAlumniSection";
+export { GrantsApplySection } from "./GrantsApplySection";
+export { GrantsAreasFundSection } from "./GrantsAreasFundSection";
+export { GrantsWhoWeSupportSection } from "./GrantsWhoWeSupportSection";
