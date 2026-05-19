b11a7277fbe528cacd949f7722e55a1c9a573e21
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-05-19 14:30:00 +0000

    feat: add supporters components barrel export

---
+++ src/app/components/supporters/index.ts
@@ -0,0 +1,7 @@
+export { SupportersFoundingDonorsSection } from "./SupportersFoundingDonorsSection";
+export { SupportersGlobeSection } from "./SupportersGlobeSection";
+export { SupportersIntroSection } from "./SupportersIntroSection";
+export { SupportersOperationsMaintainerSection } from "./SupportersOperationsMaintainerSection";
+export { SupportersProgramSupportersSection } from "./SupportersProgramSupportersSection";
+export { SupportersSectionTitleSection } from "./SupportersSectionTitleSection";
+export { SupportersSupportUsSection } from "./SupportersSupportUsSection";
