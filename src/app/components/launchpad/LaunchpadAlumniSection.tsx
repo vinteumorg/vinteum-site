68935b6fdcb4ec7fdade496d2689101bbd9d9721
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-06-29 02:05:08 -0300

    feat: add social links and funded-by badge to LaunchpadAlumniSection
    Populate social links (github, twitter, website, nostr, linkedin,
    youtube, blog) for all 26 members across cohort 1 and cohort 2.
    Add fundedBy field showing the grant source (OpenSats, Vinteum,
    BDK Foundation) for funded members.

--- src/app/components/launchpad/LaunchpadAlumniSection.tsx
+++ src/app/components/launchpad/LaunchpadAlumniSection.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, startTransition } from "react";
 import { useLanguage } from "@/contexts/LanguageContext";
-import { TeamMemberCard } from "../shared/TeamMemberCard";
+import { SocialLinks, TeamMemberCard } from "../shared/TeamMemberCard";
 import { SectionTitle } from "../shared/SectionTitle";
 
 function shuffled<T>(arr: T[]): T[] {
@@ -18,6 +18,8 @@ interface AlumniEntry {
     nameKey: string;
     roleKey: string;
     image: string;
+    fundedBy?: string;
+    links?: SocialLinks;
 }
 
 const COHORT_1: AlumniEntry[] = [
@@ -25,66 +27,120 @@ const COHORT_1: AlumniEntry[] = [
         nameKey: "fellowship.fellows.members.lucasBalieiro.name",
         roleKey: "fellowship.fellows.members.lucasBalieiro.role",
         image: "/assets/images/people/fellows/lucasbalieiro.jpg",
+        fundedBy: "OpenSats",
+        links: {
+            github: "https://github.com/lucasbalieiro",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.joao.name",
         roleKey: "fellowship.fellows.members.joao.role",
         image: "/assets/images/people/fellows/joao.jpg",
+        fundedBy: "OpenSats",
+        links: {
+            github: "https://github.com/jaoleal",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.luis.name",
         roleKey: "fellowship.fellows.members.luis.role",
         image: "/assets/images/people/fellows/luis.png",
+        fundedBy: "BDK Foundation",
     },
     {
         nameKey: "fellowship.fellows.members.luca0x46.name",
         roleKey: "fellowship.fellows.members.luca0x46.role",
         image: "/assets/images/people/fellows/luca0x46.jpg",
+        links: {
+            website: "https://luca0x46.com/",
+            github: "https://github.com/luca0x46",
+            twitter: "https://x.com/luca0x46",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.gustavoStingelin.name",
         roleKey: "fellowship.fellows.members.gustavoStingelin.role",
         image: "/assets/images/people/fellows/gustavostingelin.jpg",
+        links: {
+            github: "https://github.com/GustavoStingelin",
+            twitter: "https://x.com/gustingelin",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.moisePompilio.name",
         roleKey: "fellowship.fellows.members.moisePompilio.role",
         image: "/assets/images/people/fellows/moisespompilio.jpg",
+        fundedBy: "OpenSats",
+        links: {
+            github: "https://github.com/moisesPompilio",
+            twitter: "https://x.com/maicomjt",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.erickCestari.name",
         roleKey: "fellowship.fellows.members.erickCestari.role",
         image: "/assets/images/people/fellows/erickcestari.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            website: "https://erickcestari.dev/",
+            github: "https://github.com/erickcestari",
+            twitter: "https://x.com/iamflops",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.guilhermeMartins.name",
         roleKey: "fellowship.fellows.members.guilhermeMartins.role",
         image: "/assets/images/people/fellows/guilhermemartins.jpg",
+        fundedBy: "OpenSats",
+        links: {
+            github: "https://github.com/qlrd",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.theMhv.name",
         roleKey: "fellowship.fellows.members.theMhv.role",
         image: "/assets/images/people/fellows/themhv.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/TheMhv",
+            twitter: "https://x.com/TheMhv",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.joaozinho.name",
         roleKey: "fellowship.fellows.members.joaozinho.role",
         image: "/assets/images/people/fellows/joaozinho.jpg",
+        fundedBy: "OpenSats",
+        links: {
+            github: "https://github.com/joaozinhom",
+            nostr: "https://primal.net/p/nprofile1qqs0xwjg3fwjrqm3anxlgq0t48wg8n9zp8007ryl034nckyljdaktmqm4mmyv",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.caio.name",
         roleKey: "fellowship.fellows.members.caio.role",
         image: "/assets/images/people/fellows/caio.jpg",
+        links: {
+            github: "https://github.com/kiocos",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.lang.name",
         roleKey: "fellowship.fellows.members.lang.role",
         image: "/assets/images/people/fellows/lang.jpg",
+        links: {
+            github: "https://github.com/gusttav-lang",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.thgOO.name",
         roleKey: "fellowship.fellows.members.thgOO.role",
         image: "/assets/images/people/fellows/thgoo.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/thgO-O",
+            twitter: "https://x.com/thgO_O",
+        },
     },
 ];
 
@@ -93,66 +149,139 @@ const COHORT_2: AlumniEntry[] = [
         nameKey: "fellowship.fellows.members.viniciusCestari.name",
         roleKey: "fellowship.fellows.members.viniciusCestari.role",
         image: "/assets/images/people/fellows/viniciuscestari.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/viniciusCestarii",
+            twitter: "https://x.com/VCestarii",
+            website: "https://viniciuscestari.dev/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.chrisG.name",
         roleKey: "fellowship.fellows.members.chrisG.role",
         image: "/assets/images/people/fellows/chrisg.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/csgui",
+            twitter: "https://x.com/csgui",
+            website: "https://imchris.me/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.johnnySantos.name",
         roleKey: "fellowship.fellows.members.johnnySantos.role",
         image: "/assets/images/people/fellows/johnnysantos.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/johnnyasantoss",
+            twitter: "https://twitter.com/johnnyasantos",
+            nostr: "https://njump.me/npub1j0nyf54ndc6keq80xfyeqfumn2l23e3k79f33x5jsqxnut6a8dyqp74mu0",
+            website: "https://johnnyasantos.com/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.bc1cindy.name",
         roleKey: "fellowship.fellows.members.bc1cindy.role",
         image: "/assets/images/people/fellows/bc1cindy.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/bc1cindy",
+            twitter: "https://twitter.com/bc1cindy",
+            youtube: "https://www.youtube.com/@CindyBTC",
+            website: "https://linktr.ee/CindyBTC",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.caioUeda.name",
         roleKey: "fellowship.fellows.members.caioUeda.role",
         image: "/assets/images/people/fellows/caioueda.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/KY-U",
+            twitter: "https://x.com/KYU2029255",
+            linkedin: "https://www.linkedin.com/in/caio-ueda-sampaio-23511b253/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.allocz.name",
         roleKey: "fellowship.fellows.members.allocz.role",
         image: "/assets/images/people/fellows/allocz.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/allocz",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.octavioLucca.name",
         roleKey: "fellowship.fellows.members.octavioLucca.role",
         image: "/assets/images/people/fellows/octaviolucca.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            website: "https://octavio.xyz/",
+            github: "https://github.com/octaviolucca",
+            twitter: "https://x.com/octavio_lucca",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.victorAndre.name",
         roleKey: "fellowship.fellows.members.victorAndre.role",
         image: "/assets/images/people/fellows/victorandre.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/victorandre957",
+            blog: "https://victorandre957.github.io/personal_blog/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.isaqueFranklin.name",
         roleKey: "fellowship.fellows.members.isaqueFranklin.role",
         image: "/assets/images/people/fellows/isaquefranklin.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/IsaqueFranklin",
+            twitter: "https://x.com/IsaqueFranklin0",
+            website: "https://www.harlock.xyz/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.lucasLima.name",
         roleKey: "fellowship.fellows.members.lucasLima.role",
         image: "/assets/images/people/fellows/lucaslima.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/lucasdbr05",
+            twitter: "https://x.com/Lucaslgol05",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.renatoBritto.name",
         roleKey: "fellowship.fellows.members.renatoBritto.role",
         image: "/assets/images/people/fellows/renatobritto.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/satsfy",
+            twitter: "https://x.com/0xsatsfy",
+            website: "https://satsfy.xyz/",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.m4ycon.name",
         roleKey: "fellowship.fellows.members.m4ycon.role",
         image: "/assets/images/people/fellows/m4ycon.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/m4ycon",
+        },
     },
     {
         nameKey: "fellowship.fellows.members.jayrMotta.name",
         roleKey: "fellowship.fellows.members.jayrMotta.role",
         image: "/assets/images/people/fellows/jayrmotta.jpg",
+        fundedBy: "Vinteum",
+        links: {
+            github: "https://github.com/jayrmotta",
+            twitter: "https://x.com/jayrmotta",
+        },
     },
 ];
 
@@ -169,6 +298,8 @@ function CohortGrid({ members }: { members: AlumniEntry[] }) {
                         image={m.image}
                         name={t(m.nameKey)}
                         role={t(m.roleKey)}
+                        fundedBy={m.fundedBy}
+                        links={m.links}
                     />
                 </div>
             ))}
