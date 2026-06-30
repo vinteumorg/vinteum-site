b1f825191727b80a81164635103f7497d0eb197f
Cristian Solutchak <cristiansolutchak@gmail.com>
2026-06-30 02:03:04 -0300

    feat(newsletter): convert to server component and fetch newsletters from Ghost

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -1,13 +1,14 @@
-"use client";
-
 import { NewsletterHeroSection } from "../components/newsletter/NewsletterHeroSection";
 import { NewsletterFormSection } from "../components/newsletter/NewsletterFormSection";
+import { getNewsletters } from "@/lib/ghost/admin";
 
-export default function NewsletterPage() {
+export default async function NewsletterPage() {
+    const newsletters = await getNewsletters().catch(() => []);
+
     return (
         <div className="flex flex-col">
             <NewsletterHeroSection />
-            <NewsletterFormSection />
+            <NewsletterFormSection newsletters={newsletters} />
         </div>
     );
 }
